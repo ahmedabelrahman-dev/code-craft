c91f530af529d8595d6f4de7d2c06572f5080a08
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-26 21:16:04 +0400

    refactor the app structure and the home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import { type Metadata } from 'next';
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ConvexClientProvider from '../components/providers/ConvexClientProvider';
+import { Toaster } from 'react-hot-toast';
+import Footer from '../components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,22 +30,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-gray-100 flex flex-col`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           <ConvexClientProvider>{children}</ConvexClientProvider>
+          <Footer />
+
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
