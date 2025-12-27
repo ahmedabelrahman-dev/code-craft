3f39235e01c7c873454deb0e961a6c3be836b60d
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-28 00:32:04 +0400

    fix : convex/clerk client provider , not saving the codeExecutions in the data base

--- components/providers/ConvexClientProvider.tsx
+++ components/providers/ConvexClientProvider.tsx
@@ -6,12 +6,20 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
+function ConvexProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+}
+
 function ConvexClientProvider({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      <ConvexProvider>
         {children}
-      </ConvexProviderWithClerk>
+      </ConvexProvider>
     </ClerkProvider>
   );
 }
